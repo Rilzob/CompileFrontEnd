21c897d8f77dc7018432d3115ade84b6d0f95f57
Rilzob <watermirrorsir@163.com>
2018-11-01 18:21:43 +0800

    词法分析器 2.0

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -13,12 +13,12 @@ OPERATOR_LIST = ['+', '-', '*', '%', '/', '|', '&', '<', '>', '=', '!']
 class Lexer(object):
     def __init__(self):
         self.sourcecode = []  # 源代码
-        self.iT = []   # 标识符
-        self.CT = []   # 常数
-        self.sT = []   # 字符串
-        self.KT = []   # 关键字
-        self.PT = []   # 界符
-        self.ST = []   # 操作符
+        self.iT = set()   # 标识符
+        self.CT = set()   # 常数
+        self.sT = set()   # 字符串
+        self.KT = set()   # 关键字
+        self.PT = set()   # 界符
+        self.ST = set()   # 操作符
 
     def load_file(self, path):  # 读入源代码并对其进行处理
         file = codecs.open(path, 'r', 'utf-8')
@@ -47,6 +47,7 @@ class Lexer(object):
 
     @staticmethod
     def state_change(state_before, current_char):
+        global state_now
         if state_before == 0:
             if current_char.isalpha():  # 检查当前字符是否是字母
                 state_now = 1
@@ -62,6 +63,7 @@ class Lexer(object):
                 state_now = 0
             else:
                 Lexer.lexical_error(current_char)
+                return False
         elif state_before == 1:
             if current_char.isalpha() or current_char.isdigit():
                 state_now = state_before
@@ -72,6 +74,8 @@ class Lexer(object):
                 state_now = state_before
             elif current_char.isalpha():
                 Lexer.lexical_error(current_char)  # 数字后面紧跟字母报错
+                print("标识符不能以数字开头")
+                return False
             else:
                 state_now = 6
         elif state_before == 3:
@@ -88,6 +92,7 @@ class Lexer(object):
                 state_now = 9
         return state_now
 
+    @property
     def lexer_scanner(self):
         if self.sourcecode is None:
             return
@@ -95,17 +100,18 @@ class Lexer(object):
         state_now = 0
         value = 0
         word = string = op = ''
-        ST_len = KT_len = sT_len = CT_len = PT_len = iT_len = 0
 
         def solve_state4():
-            nonlocal ST_len, op, state_now, PT_len
-            if Lexer.is_separator(current_char):
-                self.PT.append((current_char, 'PT', PT_len))
-                PT_len += 1
+            nonlocal op, state_now
+            if state_now == (4 or 11):
+                if Lexer.is_separator(current_char):
+                    self.PT.add(current_char)
+                    state_now = 0
+                elif Lexer.is_operator(current_char):
+                    op += current_char
+                    state_now = 11
+            else:
                 state_now = 0
-            elif Lexer.is_operator(current_char):
-                op += current_char
-                state_now = 11
 
         for line in self.sourcecode:
             for char in line:
@@ -113,6 +119,9 @@ class Lexer(object):
         for current_char in char_list:
             state_before = state_now
             state_now = self.state_change(state_before, current_char)
+            if not state_now:
+                print("代码格式存在错误！")
+                return False
             if state_now == 1:
                 word += current_char
             elif state_now == 2:
@@ -123,40 +132,28 @@ class Lexer(object):
                 if state_now == 11:
                     op += current_char
                 elif state_now == 4:
-                    self.PT.append((current_char, 'PT', PT_len))
-                    PT_len += 1
+                    self.PT.add(current_char)
                     state_now = 0
                 elif state_now == 5:
                     if self.is_keyword(word):
-                        self.KT.append((word, 'KT', KT_len))
-                        KT_len += 1
+                        self.KT.add(word)
                     else:
-                        self.iT.append((word, 'iT', iT_len))
-                        iT_len += 1
+                        self.iT.add(word)
                     state_now = self.state_change(0, current_char)
                     word = ''
-                    if state_now == (4 or 11):
-                        solve_state4()
-                    else:
-                        state_now = 0
+                    solve_state4()
                 elif state_now == 6:
-                    self.CT.append((value, 'CT', CT_len))
-                    CT_len += 1
+                    self.CT.add(value)
                     state_now = self.state_change(0, current_char)
                     value = 0
-                    if state_now == (4 or 11):
-                        solve_state4()
-                    else:
-                        state_now = 0
+                    solve_state4()
                 elif state_now == 7:
                     string += current_char
-                    self.sT.append((string, 'sT', sT_len))
-                    sT_len += 1
+                    self.sT.add(string)
                     string = ''
                     state_now = 0
                 elif state_now == 9:
-                    self.ST.append((op, 'ST', ST_len))
-                    ST_len += 1
+                    self.ST.add(op)
                     op = ''
                     state_now = self.state_change(0, current_char)
                     if state_now == 3:
@@ -168,7 +165,7 @@ class Lexer(object):
 if __name__ == '__main__':
     Lexer = Lexer()
     Lexer.load_file('/Users/rilzob/PycharmProjects/CompileFrontEnd/SourceCode1.txt')
-    Lexer.lexer_scanner()
+    Lexer.lexer_scanner
     print("iT:")
     for iT in Lexer.iT:
         print(iT)
